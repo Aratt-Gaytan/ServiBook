06f975ca47a1df6e090360eb462d5524f7d65cd2
Aratt-Gaytan <aratt.gaytan.flores0@gmail.com>
2024-08-04 00:06:46 -0600

    Add production warning false

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,6 +73,7 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+  config.active_record.sqlite3_production_warning=false
 
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
